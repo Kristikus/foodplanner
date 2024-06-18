f89508cffc1444ca73cb251a435d2ee1e5466a23
Kristina Kuster <kriskus@live.fr>
2024-06-18 14:03:47 +0200

    Get random recipes for breakfast and main dishes for the lunch and diner

--- planning.js
+++ planning.js
@@ -1,3 +1,5 @@
+import { getData } from './fetchData.js';
+
 document.addEventListener('DOMContentLoaded', function () {
   const daysOfWeek = [
     'Monday',
@@ -18,23 +20,23 @@ document.addEventListener('DOMContentLoaded', function () {
                     <h2 class="card-header bk-secondary text-white rounded-top-4">
                         ${day}
                     </h2>
-                    <ul class="cards-recipes list-group list-group-flush">
+                    <ul class="list-group list-group-flush">
                     <h3 class="fs-5 mb-0 mt-3 fst-italic">Breakfast :</h3>
-                    <li class="card-recipes list-group-item no-border"></li>
+                    <li class="breakfast list-group-item no-border"></li>
                     <a class="icon-link" href="#">
                         <img class="m-auto mb-3 mt-2 img-thumbnail" src="./img/arrow-repeat.svg"
                             alt="icon for generate a new recipe">
                         </a>
                     <hr>
                     <h3 class="fs-5 mb-0 mt-3 fst-italic">Lunch :</h3>
-                    <li class="card-recipes list-group-item no-border"></li>
+                    <li class="main-course list-group-item no-border"></li>
                     <a class="icon-link" href="#">
                         <img class="m-auto mb-3 mt-2 img-thumbnail" src="./img/arrow-repeat.svg"
                             alt="icon for generate a new recipe">
                     </a>
                     <hr>
                     <h3 class="fs-5 mb-0 mt-3 fst-italic">Diner :</h3>
-                    <li class="card-recipes list-group-item no-border"></li>
+                    <li class="main-course list-group-item no-border"></li>
                     <a class="icon-link" href="#">
                         <img class="m-auto mb-3 mt-2 img-thumbnail" src="./img/arrow-repeat.svg"
                             alt="icon for generate a new recipe">
@@ -45,19 +47,25 @@ document.addEventListener('DOMContentLoaded', function () {
         `;
   });
 
-  function getRecipes() {
-    return fetch('data.json')
-      .then((response) => response.json())
+  // const baseUrl ='https://api.spoonacular.com/recipes/random?apiKey=1c33e2baebbf45ce857291cce918fa18';
+
+  function getRecipes(url, selector) {
+    return getData(url)
+      .then((data) => {
+        data = data.recipes;
+
+        const recipeLocations = document.querySelectorAll(selector);
+
+        recipeLocations.forEach((recipeLocation, index) => {
+          const recipe = data[index];
+          recipeLocation.textContent = recipe.title;
+        });
+      })
       .catch((error) => {
-        console.error('Erreur lors du chargement du fichier JSON :', error);
-    });
+        console.error(error.message);
+      });
   }
 
-  getRecipes().then((recipes) => {
-    const emplacements = document.querySelectorAll('.card-recipes');
-    emplacements.forEach((emplacement, index) => {
-      const recipe = recipes[index];
-      emplacement.textContent = recipe.titre;
-    });
-  });
+  getRecipes(`${baseUrl}&number=7&include-tags=breakfast`, '.breakfast');
+  getRecipes(`${baseUrl}&number=14&include-tags=main%20course`, '.main-course');
 });
